d27037b3c081a208dfda68e043dcbd6bfaf1de77
meraz <merazhossenmh@gmail.com>
2022-11-12 16:34:32 +0600

    tour

---
+++ src/Tours.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Tour from './tour'
+
+
+const Tours = ({tours,removeTour}) => {
+  return (
+    <section>
+      <div className="title">
+        <h2>
+          Our Tours 
+        </h2>
+        <div className="underline"></div>
+      </div>
+      <div>
+        {tours.map((tour)=>{
+          console.log(tour)
+           return <Tour key={tour.id} {...tour} removeTour={removeTour}  />
+        })}
+      </div>
+    </section>
+  )
+}
+
+export default Tours
